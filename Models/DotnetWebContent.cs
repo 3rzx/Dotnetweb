3158033da648cae92d2d641372bdccedc388e5bc
Zoe Chen <zoechen@cybersoft4u.com>
2019-03-22 16:31:30 +0800

    First commit

---
+++ Models/DotnetWebContent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetWeb.Models
+{
+    public class DotnetWebContent : DbContext
+    {
+        public DotnetWebContent(DbContextOptions<DotnetWebContent> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ServiceMapping> ServiceMappings { get; set; }
+    }
+}
---
+++ Models/ServiceMapping.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetWeb.Models
+{
+    public class ServiceMapping
+    {
+        [Required]
+        public string ServiceId { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+    }
+}
